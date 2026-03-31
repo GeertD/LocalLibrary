1ca075f2975525a408d9d526b382a6a643fe34b8
Geert Dierick <geert2dierick@gmail.com>
2026-03-31 17:09:44 +0200

    Add homepage, book and author pages

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
+from catalog.models import Author, Book, BookInstance, Genre
+
 
 def index(request):
-    return HttpResponse('Index page')
+    """Homepage."""
 
-def say_hello(request):
-    return HttpResponse('Hello World')
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+
+    context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+        "num_genres": num_genres,
+    }
+
+    return render(request, "catalog/index.html", context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
